c65433e6d8baa59a89dff1fbc14a3ce584e41241
rogerbaiges <rogerbaigestrilla@gmail.com>
2024-08-09 22:15:48 +0200

    FIX close Streamlit server automatically

--- assets/CatGPT_run.py
+++ assets/CatGPT_run.py
@@ -1,15 +1,24 @@
 import subprocess
 import time
 import os
+import signal
 
 def run_streamlit():
     # Obtain the path to the main.py file
-    main_py_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'main.py')
-    # Run Streamlit
+    main_py_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'CatGPT_app.py')
+    
+    # Run Streamlit as a subprocess
     cmd = ["streamlit", "run", main_py_path]
-    subprocess.Popen(cmd)
-    # Wait for the server to start
-    time.sleep(2)
+    process = subprocess.Popen(cmd)
+
+    try:
+        # Wait for the server to start
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        # Terminate the Streamlit process on Ctrl+C
+        process.send_signal(signal.SIGINT)
+        process.wait()
 
 if __name__ == "__main__":
     run_streamlit()
